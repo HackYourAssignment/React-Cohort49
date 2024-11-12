a44e38b15529fc7748da3779c3a89f759a398425
Alia Taher <alia.taher022@gmail.com>
2024-11-12 21:06:04 +0100

    feat: enhance Vite configuration with server headers and resolve extensions

--- week1/project/ecommerce/vite.config.js
+++ week1/project/ecommerce/vite.config.js
@@ -1,8 +1,16 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce/'
+  base: '/ecommerce/',
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
 })
