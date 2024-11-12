b18943d6d3648c668e65755bcde1f92df595727b
Alia Taher <alia.taher022@gmail.com>
2024-11-12 21:21:44 +0100

    fix: update script path in index.html and enhance Vite configuration

--- week1/project/ecommerce/vite.config.js
+++ week1/project/ecommerce/vite.config.js
@@ -1,16 +1,23 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/ecommerce/',
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx']
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
   }
 })
