78ed95153a0fa95e6dcd6e0bc6790b43ccb55562
Alia Taher <alia.taher022@gmail.com>
2024-11-16 12:50:21 +0100

    enable format on save in VSCode settings

--- week1/project/ecommerce/src/App.jsx
+++ week1/project/ecommerce/src/App.jsx
@@ -1,11 +1,9 @@
 import NavBar from "./components/NavBar";
 
-
 function App() {
   return (
-    <div className="main-Page"> 
+    <div className="main-Page">
       <NavBar />
-      
     </div>
   );
 }
--- week1/project/ecommerce/src/components/Categories.jsx
+++ week1/project/ecommerce/src/components/Categories.jsx
@@ -5,14 +5,14 @@ import allProducts from "../fake-data/all-products.js";
 import { useState } from "react";
 
 const Categories = () => {
-
- 
-
   const normalizeCategoryName = (categoryName) => {
-    return categoryName.replace(/^FAKE:\s*/, "").trim().toLowerCase();
+    return categoryName
+      .replace(/^FAKE:\s*/, "")
+      .trim()
+      .toLowerCase();
   };
 
- const [activeCategory, setActiveCategory] = useState(null);
+  const [activeCategory, setActiveCategory] = useState(null);
 
   // Function to toggle category
   const toggleCategory = (category) => {
@@ -23,24 +23,31 @@ const Categories = () => {
 
   // Filter products based on active category
   const filteredProducts = activeCategory
-    ? allProducts.filter((product) => normalizeCategoryName(product.category)  === normalizeCategoryName(activeCategory))
+    ? allProducts.filter(
+        (product) =>
+          normalizeCategoryName(product.category) ===
+          normalizeCategoryName(activeCategory)
+      )
     : allProducts;
-
-    
 
   return (
     <div>
       <div className="Categories-container navbar">
         {allCategories.map((category, index) => {
           return (
-            <Category key={index} title={category} onClick={toggleCategory} isActive={activeCategory=== category} />
+            <Category
+              key={index}
+              title={category}
+              onClick={toggleCategory}
+              isActive={activeCategory === category}
+            />
           );
         })}
-        </div>
+      </div>
 
-        <div className="all-products-container">
+      <div className="all-products-container">
         <AllProductsListing products={filteredProducts} />
-        </div>
+      </div>
     </div>
   );
 };
