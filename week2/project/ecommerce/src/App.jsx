96bd985fa8947a228ca6cd46dfda9d0fca81f9e9
Mustafa Sarıtaş <mstfsrts@gmail.com>
2024-11-19 20:08:52 +0100

    React Week2 Assignment Completed

---
+++ week2/project/ecommerce/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import "./styles/index.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
